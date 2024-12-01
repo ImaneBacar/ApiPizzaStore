01d8b5de58c169b2454cfe53e929c56d9034c960
BacarImane2 <imanebacar@outlook.fr>
2024-12-01 10:26:50 +0000

    ajout et test des fonctionnalité , get,put,delete

--- Program.cs
+++ Program.cs
@@ -26,5 +26,24 @@ var builder = WebApplication.CreateBuilder(args);
             await db.SaveChangesAsync();    
             return Results.Created($"/pizza/{pizza.IdEhod}", pizza); 
             });
-
+    app.MapGet("/pizza/{id}", async (PizzaDb db, int id) => await db.Pizzas.FindAsync(id));
+    app.MapPut("/pizza/{id}", async (PizzaDb db, Pizza updatepizza, int id) =>{
+            var pizza = await db.Pizzas.FindAsync(id);
+            if (pizza is null) return Results.NotFound();
+            pizza.NomEhod = updatepizza.NomEhod;
+            pizza.DescriptionEhod = updatepizza.DescriptionEhod;
+            await db.SaveChangesAsync();
+            return Results.NoContent();
+            });
+    app.MapDelete("/pizza/{id}", async (PizzaDb db, int id) =>
+            {
+            var pizza = await db.Pizzas.FindAsync(id);
+            if (pizza is null)
+            {
+            return Results.NotFound();
+            }
+            db.Pizzas.Remove(pizza);
+            await db.SaveChangesAsync();
+            return Results.Ok();
+            });
     app.Run();
--- obj/Debug/net8.0/PizzaStore.AssemblyInfo.cs
+++ obj/Debug/net8.0/PizzaStore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PizzaStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e6e4c21cd6821eb7c83e60beafa7bd742e755d3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c70de04b5342728f69e947180e482b8aa81b75b")]
 [assembly: System.Reflection.AssemblyProductAttribute("PizzaStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PizzaStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
